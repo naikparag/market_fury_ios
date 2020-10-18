43cd4c2471ebe814437cc75ecb4ca3dce09650a6
Parag Naik <naikparag@gmail.com>
2020-10-18 23:09:57 +0530

    Initial Commit

---
+++ MarketFury/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MarketFury
+//
+//  Created by Naik, Parag Laxman on 18/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarketFury/MarketFuryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarketFuryApp.swift
+//  MarketFury
+//
+//  Created by Naik, Parag Laxman on 18/10/20.
+//
+
+import SwiftUI
+
+@main
+struct MarketFuryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
